01219a59d6f417de6060615fd0bfb8bb8c8a17a8
vanshsaini8955-ui <vanshsaini8955@gmail.com>
2026-07-06 20:26:31 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char op;
+
+    cin >> a >> op >> b;
+
+    switch(op) {
+        case '+':
+            cout << a + b;
+            break;
+
+        case '-':
+            cout << a - b;
+            break;
+
+        case '*':
+            cout << a * b;
+            break;
+
+        case '/':
+            cout << a / b;
+            break;
+
+        default:
+            cout << "Invalid Operator";
+    }
+
+    return 0;
+}
---
+++ countDigits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    cin >> n;
+
+    while(n > 0) {
+        count++;
+        n = n / 10;
+    }
+
+    cout << "Digits = " << count;
+
+    return 0;
+}
